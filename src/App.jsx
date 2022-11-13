ca17b137abb63615ad244f7ea556e1cd6eb9f860
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-11-13 16:19:40 +0000

    like button

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Amplify } from 'aws-amplify';
+import { API, graphqlOperation } from 'aws-amplify';
 import awsconfig from './aws-exports';
-// import { listSongs } from './graphql/queries';
-
+import { listSongs } from './graphql/queries';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react-v1';
+import { updateSong } from './graphql/mutations';
 import './App.css';
 
 Amplify.configure(awsconfig);
 
 function App() {
-  // const [songs, setSongs] = useState([]);
+  const [songs, setSongs] = useState([]);
 
-  // const fetchSongs = async () => {
-  //   try {
-  //     const songData = await API.graphql(graphqlOperation(listSongs));
-  //     const songList = songData.data.listSongs.items;
-  //     console.log('song list', songList);
-  //     setSongs(songList);
-  //   } catch (error) {
-  //     console.log('error on fetching songs', error);
-  //   }
-  // };
+  const fetchSongs = async () => {
+    try {
+      const songData = await API.graphql(graphqlOperation(listSongs));
+      const songList = songData.data.listSongs.items;
+      console.log('song list', songList);
+      setSongs(songList);
+    } catch (error) {
+      console.log('error on fetching songs', error);
+    }
+  };
 
-  // useEffect(() => {
-  //   fetchSongs();
-  // }, []);
+  const addLike = async (idx) => {
+    try {
+      const song = songs[idx];
+      song.likes = song.likes + 1;
+      delete song.createdAt;
+      delete song.updatedAt;
+
+      const songData = await API.graphql(
+        graphqlOperation(updateSong, { input: song })
+      );
+      const songList = [...songs];
+      songList[idx] = songData.data.updateSong;
+      setSongs(songList);
+    } catch (error) {
+      console.log('error on adding Like to song', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
 
   return (
     <div className='App'>
@@ -32,6 +51,33 @@ function App() {
         <AmplifySignOut />
         <h2>App Content</h2>
       </header>
+      <div className='songList'>
+        {songs.map((song, idx) => {
+          return (
+            <div key={`song${idx}`}>
+              <div className='songCard'>
+                <div aria-label='play'>Play Button</div>
+                <div>
+                  <div className='songTitle'>{song.title}</div>
+                  <div className='songOwner'>{song.owner}</div>
+                </div>
+                <div>
+                  <div
+                    aria-label='like'
+                    onClick={() => {
+                      addLike(idx);
+                    }}
+                  >
+                    Favourite Icon
+                  </div>
+                  {song.likes}
+                </div>
+                <div className='songDescription'>{song.description}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
