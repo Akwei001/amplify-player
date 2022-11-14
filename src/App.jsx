5d7e5c7fc5940ac8c0838e5b3e847624056210d4
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-11-14 12:48:59 +0000

    S3 get working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Amplify } from 'aws-amplify';
-import { API, graphqlOperation } from 'aws-amplify';
+import { API, graphqlOperation, Storage } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { listSongs } from './graphql/queries';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react-v1';
@@ -11,6 +11,8 @@ Amplify.configure(awsconfig);
 
 function App() {
   const [songs, setSongs] = useState([]);
+  const [songPlaying, setSongPlaying] = useState('');
+  const [audioURL, setAudioURL] = useState('');
 
   const fetchSongs = async () => {
     try {
@@ -41,6 +43,27 @@ function App() {
     }
   };
 
+  const toggleSong = async (idx) => {
+    if (songPlaying === idx) {
+      setSongPlaying('');
+      return;
+    }
+    // setSongPlaying(idx);
+    const songFilePath = songs[idx].filePath;
+
+    try {
+      const fileAccessURL = Storage.get(songFilePath, { expires: 60 });
+      console.log('access url', fileAccessURL);
+      setSongPlaying(idx);
+      setAudioURL(fileAccessURL);
+      return;
+    } catch (error) {
+      console.error('error accessing the file from s3', error);
+      setAudioURL('');
+      setSongPlaying('');
+    }
+  };
+
   useEffect(() => {
     fetchSongs();
   }, []);
@@ -56,7 +79,9 @@ function App() {
           return (
             <div key={`song${idx}`}>
               <div className='songCard'>
-                <div aria-label='play'>Play Button</div>
+                <div aria-label='play' onClick={() => toggleSong(idx)}>
+                  {songPlaying === idx ? 'Pause Icon' : 'Play Icon'}
+                </div>
                 <div>
                   <div className='songTitle'>{song.title}</div>
                   <div className='songOwner'>{song.owner}</div>
