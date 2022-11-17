e38deb425cf5d2a55f7b54b43685200051bd400c
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-11-17 19:07:36 +0000

    file upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import { API, graphqlOperation, Storage } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { listSongs } from './graphql/queries';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react-v1';
-import { updateSong } from './graphql/mutations';
+import { updateSong, createSong } from './graphql/mutations';
 import ReactPlayer from 'react-player/lazy';
 import { BsPlayFill, BsPauseFill } from 'react-icons/bs';
 import { MdFavorite, MdPublish } from 'react-icons/md';
 import { IoIosAddCircle } from 'react-icons/io';
+import { v4 as uuid } from 'uuid';
 import './App.css';
 
 Amplify.configure(awsconfig);
@@ -137,20 +138,60 @@ function App() {
 export default withAuthenticator(App);
 
 const Addsong = ({ onUpload }) => {
+  const [songData, setSongData] = useState({});
+  const [mp3Data, setMp3Data] = useState();
+
   const uploadSong = async () => {
     //Upload the song
+    console.log('songData', songData);
+    const { title, description, owner } = songData;
+
+    const { key } = await Storage.put(`${uuid()}.mp3`, mp3Data, {
+      contentType: 'audio/mp3',
+    });
+
+    const createSongInput = {
+      id: uuid(),
+      title,
+      description,
+      owner,
+      filePath: key,
+      like: 0,
+    };
+
+    await API.graphql(graphqlOperation(createSong, { input: createSongInput }));
+
     onUpload();
   };
   return (
     <div className='newSong'>
-      <textarea label='title' placeholder='Title' />
-
       <textarea
-        label='
-    artist'
-        placeholder='Artist'
+        label='title'
+        placeholder='Title'
+        value={songData.title}
+        onChange={(e) => setSongData({ ...songData, title: e.target.value })}
       />
-      <textarea label='Description' placeholder='Description' />
+      <textarea
+        label='artist'
+        placeholder='Artist'
+        value={songData.artist}
+        onChange={(e) => setSongData({ ...songData, owner: e.target.value })}
+      />
+      <textarea
+        label='description'
+        placeholder='Description'
+        value={songData.description}
+        onChange={(e) =>
+          setSongData({ ...songData, description: e.target.value })
+        }
+      />
+
+      <input
+        type='file'
+        accept='audio/mp3'
+        onChange={(e) => setMp3Data(e.target.files[0])}
+      />
+
       <div onClick={uploadSong}>
         <MdPublish />
       </div>
